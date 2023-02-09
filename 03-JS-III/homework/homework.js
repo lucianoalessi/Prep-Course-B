85028d9ecf5d58afa548a3ca6b6371a493413f3f
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-02-08 23:57:27 -0300

    3er

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,24 +8,43 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+   var acumulador = 0
+   for(let i = 0 ; i <= 10 ; i++){
+    acumulador += i
+   }
+   return acumulador;
 }
+
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+var numerosPares = []
+for(let i=0 ; i <= array.length ; i++){
+  if(array[i] % 2 === 0){
+    numerosPares.push(array[i])
+  }
+}
+return numerosPares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+ 
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma = 0
+  for(let i = 0 ; i <= array.length ; i++){
+    suma += array[i]
+  }
+  return suma
 }
 
 function numeroDigitos(num){
